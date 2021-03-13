91ad051d5f0b0492e37533aa08177844dbe42e78
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-03-13 12:40:21 -0700

    API fixes
    most api calls work now

--- Movies.js
+++ Movies.js
@@ -1,23 +1,51 @@
-var mongoose1 = require('mongoose');
-var Schema = mongoose1.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
-mongoose1.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
+//connect method
 //mongoose.connect(process.env.DB, { useNewUrlParser: true });
 try {
-    mongoose1.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+    mongoose.connect( process.env.MONGODB_URI, () =>
         console.log("connected"));
 }catch (error) {
     console.log("could not connect");
 }
-mongoose1.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true);
+
+//movie schema
 var MovieSchema = new Schema({
-    Title: {type:String, required: true},
-    Year: { type: String, required: true},
-    Genre: { type: String, required: true, enum:['Action', 'Adventure',  'Comedy',  'Drama',  'Fantasy',  'Horror',  'Mystery',  'Thriller', 'Western'] },
-    Actors : { type : Array,ActorName:{type:String},CharacterName:{type:String} }
+    title: { type: String},
+    yearReleased: {type: Number, min:[1900, 'Must be greater than 1899'], max:[2100,'Must be less than 2100'], required: true },
+    genre: { type:String},
+    actors: [{ actorName: String, characterName: String}]
 });
+MovieSchema.methods.find = function (err, movies)
+{
+
+  return movies
+
+}
+
+
+/*//Get/Read movies
+MovieSchema.get('get', function (next){
+    var movie = this;
+});
+//Post/Create movies
+MovieSchema.save('save', function(next) {
+    var movie = this;
+    next();
+});
+//Put/Update movies
+MovieSchema.update('update', function (next){
+    var movie = this;
+});
+//Delete movies
+MovieSchema.delete('delete', function (next){
+    var movie = this;
+});*/
 
 //return the model to server
-module.exports = mongoose1.model('Movie',MovieSchema);
+module.exports = mongoose.model('Movie', MovieSchema);
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var authJwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var User = require('./Users');
-
+var Movie = require('./Movies');
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -86,8 +86,111 @@ router.post('/signin', function (req, res) {
     })
 });
 
- 
+router.route('/movies')
 
+    //Retrieve movies
+    .get(function (req, res) {
+        if (!req.body.title) {
+            res.json({success: false, message: 'Please submit title of the movie you wish to find.'});
+        } else {
+            var title_query = req.body.title;
+            console.log(req.body.title);
+            Movie.findOne({title: title_query}, function (err, movie) {
+                if (err) res.send(err);
+                else
+                res.json({success: true, message: movie});
+            });
+        }
+    })
+    //Save movies
+    .post(function (req, res) {
+        if (!req.body.title || !req.body.genre || !req.body.year || !req.body.actors && req.body.actors.length) {
+            res.json({success: false, msg: 'Please pass Movie Title, Year released, Genre, and Actors(Actor Name and Character Name)'});
+        }
+        else {
+            if(req.body.actors.length < 3) {
+                res.json({ success: false, message: 'Please include at least three actors.'});
+            }
+            else {
+                var movie = new Movie(req, res);
+                movie.Title = req.body.title;
+                movie.Year = req.body.year;
+                movie.Genre = req.body.genre;
+                movie.Actors= req.body.actors;
+
+                movie.save(function(err) {
+                    if (err) {
+                        if (err.code == 11000)
+                            return res.json({ success: false, message: 'A movie with that title already exists.'});
+                        else
+                            return res.send(err);
+                    }
+                    res.json({ message: 'Movie successfully created.' });
+                });
+            }
+        }
+    })
+
+    .put(function(req, res) {
+        if (!req.body.title){
+            res.json({success: false, message: 'Please submit title of the movie you wish to update.'});
+        } else {
+            var title = req.body.title;
+            Movie.findOne({title:title},function(err,movie){
+                if(movie != null){
+                    if (err) res.send(err);
+
+                    if(req.body.year){
+                        movie.year = req.body.year;
+                    }
+
+                    if(req.body.genre){
+                        movie.genre = req.body.genre;
+                    }
+
+                    if(req.body.actor_1){
+                        movie.actors[0][0] = req.body.actor_1;
+                    }
+                    if(req.body.actor_2){
+                        movie.actors[1][0] = req.body.actor_2;
+                    }
+                    if(req.body.actor_3){
+                        movie.actors[2][0] = req.body.actor_3;
+                    }
+                    if(req.body.character_1){
+                        movie.actors[0][1] = req.body.character_1;
+                    }
+                    if(req.body.character_2){
+                        movie.actors[1][1] = req.body.character_2;
+                    }
+                    if(req.body.character_3){
+                        movie.actors[2][1] = req.body.character_3;
+                    }
+
+                    movie.save(function(err){
+                        if (err) res.send(err);
+                        res.json({success: true, message: 'Movie updated!'});
+                    });
+                }else{
+                    res.json({success: false, message: 'Failed to find movie!'});
+                }
+            });
+        }
+    })
+
+    //Delete movies
+    .delete(function(req, res) {
+        if (!req.body.title){
+            res.json({success: false, message: 'Please submit title of the movie you wish to delete.'});
+        } else {
+
+            var title = req.body.title;
+            Movie.remove({title:title}, function(err, movie) {
+                if (err) res.send(err);
+                res.json({success: true, message: 'deleted'});
+            });
+        }
+    });
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
